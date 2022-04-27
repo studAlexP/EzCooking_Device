38c8544f0779df37a2aa73a6c28cc43792b6a469
Alexander Pruka <alexander.pruka@stud.fh-campuswien.ac.at>
2022-04-27 16:26:11 +0200

    Changed save format from txt to json

--- speech.py
+++ speech.py
@@ -1,16 +1,26 @@
 import speech_recognition as sr
+import json
 
-r = sr.Recognizer()
 
-while True:
-    with sr.Microphone() as source:
-        audio = r.listen(source)
+def main():
+    r = sr.Recognizer()
 
-        try:
-            text = r.recognize_google(audio)
-            data = open("data/data.txt", "w")
-            data.write(text)
-            data.close()
-            print(text)
-        except:
-            print("Failed")
+    while True:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+            try:
+                text = r.recognize_google(audio)
+                data = open("data/data.json", "w")
+                json_text = str(text)
+                json_text = json_text.split()
+                json_data = {}
+                json_data["keywords"] = json_text
+                data.write(json.dumps(json_data))
+                data.close()
+                print(json_text)
+            except:
+                print("Failed")
+
+if __name__ == "__main__":
+    main()
